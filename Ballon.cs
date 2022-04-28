67d30ad3891bdbdb098d82a6f972184c58ad8e9a
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2022-04-28 17:25:18 +0530

    Compleate

---
+++ Ballon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballon : MonoBehaviour
+{
+   [SerializeField] private int ClikToPop = 3;
+    private void OnMouseDown()
+    {
+        ClikToPop--;
+
+        transform.localScale += new Vector3(2f, 2f, 2f);
+
+        if (ClikToPop <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
